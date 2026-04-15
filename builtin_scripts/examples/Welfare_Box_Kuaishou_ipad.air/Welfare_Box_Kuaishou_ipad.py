5ebfd82c0c17bfd75258434aabc56cd49803d52f
孤帆 <13680972988@163.coom>
2026-04-15 17:45:53 +0800

    任务管理模块-添加更多内置脚本

---
+++ builtin_scripts/examples/Welfare_Box_Kuaishou_ipad.air/Welfare_Box_Kuaishou_ipad.py
@@ -0,0 +1,26 @@
+# -*- encoding=utf8 -*-
+__author__ = "谭振捷"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+kuaishou_pos = exists(Template(r"tpl1765590200738.png", record_pos=(-0.403, -0.303), resolution=(1600, 2560)))
+if kuaishou_pos:
+    touch(kuaishou_pos)
+    touch(Template(r"tpl1765590537732.png", record_pos=(-0.407, -0.291), resolution=(1600, 2560)))
+    sleep(3)
+x_pos = exists(Template(r"tpl1765590588955.png", record_pos=(0.421, 0.29), resolution=(1600, 2560)))
+if x_pos:
+    touch(x_pos)
+    sleep(1)
+    
+make_money_pos = exists(Template(r"tpl1765590647833.png", record_pos=(0.196, 0.699), resolution=(1600, 2560)))
+if make_money_pos:
+    touch(make_money_pos)
+    sleep(2)
+    
+    
+welfare_box_pos = exists(Template(r"tpl1765590812227.png", record_pos=(0.36, 0.528), resolution=(1600, 2560)))
+if welfare_box_pos:
+    touch(welfare_box_pos)
